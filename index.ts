326fc25a0f139926699fdaf1015d53a0a46b7669
Jose Jurado <josejurado@Joses-MacBook-Pro.local>
2024-10-22 15:13:14 -0500

    Primer commit - rest server

---
+++ index.ts
@@ -0,0 +1,17 @@
+import Server from "./classes/server";
+import router from "./routes/router";
+import bodyParser from 'body-parser'
+import cors from 'cors';
+
+const server = new Server();
+
+// BodyParser
+server.app.use(bodyParser.urlencoded({ extended: true }));
+server.app.use(bodyParser.json());
+server.app.use(cors({ origin:true, credentials: true }));
+// CORS
+server.app.use('/', router)
+
+server.start( () => {
+    console.log(`El servidor esta corriendo en el puerto ${ server.port }`);
+});
